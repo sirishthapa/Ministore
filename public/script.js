b74b43fa7822fb9ef51482895889970c71534648
sirishthapa <thapacrish53@gmail.com>
2023-07-26 12:28:13 +0545

    popup design made

--- public/script.js
+++ public/script.js
@@ -19,12 +19,34 @@ decrementButton.addEventListener('click', () => {
 
 
 // Get the cross icons div
-const removeButtons = document.querySelectorAll(".td-cross")
+
+function remove(){
+
+Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
+const removeButtons = document.querySelectorAll(".swal2-confirm")
+console.log(removeButtons);
 // loop through all the divs with td-cross class
 for (let button of removeButtons) {
   // add click event listener to individual divs
   button.addEventListener("click", () => {
     // remove the tr element
-    button.parentElement.parentElement.remove()
+    document.getElementsByClassName("table-product")[0].parentElement.remove()
   })
+}
 }
